1c3162e137810b5568894918b4d425067fcf4cef
nuuuwan <nuuuwan@gmail.com>
2025-01-19 09:40:21 +0530

    Add date to super title

--- src/view/molecules/ArticleMetadataView.js
+++ src/view/molecules/ArticleMetadataView.js
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from '@mui/material';
 
 export default function ArticleMetadataView({
   articleMetadata,
@@ -10,12 +10,17 @@ export default function ArticleMetadataView({
     setActiveArticleIndex(index);
   };
   return (
-    <Box onClick={onClick} sx={{ cursor: "pointer" }}>
+    <Box onClick={onClick} sx={{ cursor: 'pointer' }}>
       <Box sx={{ p: 1 }}>
-        <Stack direction="column" gap={0} sx={{ alignItems: "top" }}>
-          <Typography variant="caption" color="#888">
-            {articleMetadata.source}
-          </Typography>
+        <Stack direction="column" gap={0} sx={{ alignItems: 'top' }}>
+          <Stack direction="row" gap={1}>
+            <Typography variant="caption" color="#888">
+              {articleMetadata.source}
+            </Typography>
+            <Typography variant="caption" color="#ccc">
+              {articleMetadata.dateStr}
+            </Typography>
+          </Stack>
           <Typography variant="h6" color={articleMetadata.color}>
             {articleMetadata.title}
           </Typography>
